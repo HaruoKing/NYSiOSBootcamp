f3051d63b662d4530090509ec0a44d2fe8dca856
VRodriguez7 <Victor.rodriguez@its.ny.gov>
2024-06-25 15:01:18 -0400

    Add Week 4 HW Project

---
+++ Week 4/Week 4 Homework/Week 4 Homework/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week 4 Homework
+//
+//  Created by Victor Rodriguez on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Week 4/Week 4 Homework/Week 4 Homework/Week_4_HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week_4_HomeworkApp.swift
+//  Week 4 Homework
+//
+//  Created by Victor Rodriguez on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Week_4_HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
