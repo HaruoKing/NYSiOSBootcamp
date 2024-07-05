d4eede83cbbb3c5ef94114e518629e56d647edc1
VRodriguez7 <Victor.rodriguez@its.ny.gov>
2024-07-05 10:39:04 -0400

    Adding week 5 homework to week 5 branch

---
+++ Week 5/Week 5 Homework/Task/View/CompletedTaskListView.swift
@@ -0,0 +1,41 @@
+//
+//  CompletedTaskListView.swift
+//  Task
+//
+//  Created by Victor Rodriguez on 7/5/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct CompletedTaskListView: View {
+    @ObservedObject var taskStore: TaskStore
+    @State private var searchText = ""
+    
+    var filteredTasks: [Task] {
+        if searchText.isEmpty {
+            return taskStore.tasks.filter { $0.isCompleted }
+        } else {
+            return taskStore.tasks.filter { $0.isCompleted && $0.title.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(filteredTasks) { task in
+                    if let taskIndex = taskStore.tasks.firstIndex(where: { $0.id == task.id }) {
+                        NavigationLink(destination: TaskDetailView(task: $taskStore.tasks[taskIndex])) {
+                            VStack {
+                                TaskRowView(task: $taskStore.tasks[taskIndex])
+                            }
+                            .padding([.leading, .trailing], 20)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Completed Tasks")
+            .searchable(text: $searchText)
+        }
+    }
+}
---
+++ Week 5/Week 5 Homework/Task/View/TaskRowView.swift
@@ -0,0 +1,45 @@
+//
+//  TaskRowView.swift
+//  Task
+//
+
+import SwiftUI
+
+struct TaskRowView: View {
+    @Binding var task: Task
+    @State private var showTemporaryIcon = false
+    
+    var body: some View {
+        HStack {
+            Text(task.title)
+            Spacer()
+            Image(systemName: showTemporaryIcon ? (task.isCompleted ? "square" : "checkmark.square") : (task.isCompleted ? "checkmark.square" : "square"))
+                .foregroundColor(showTemporaryIcon ? (task.isCompleted ? Color.red : Color.green) : (task.isCompleted ? Color.green : Color.red))
+                .onTapGesture {
+                    startAnimation()
+                }
+        }
+        .font(.title3)
+        .bold()
+        .padding([.top, .bottom], 15)
+        .padding([.leading, .trailing], 10)
+    }
+    
+    private func startAnimation() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            showTemporaryIcon = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                withAnimation(Animation.easeIn(duration: 1)) {
+                    task.isCompleted.toggle()
+                    showTemporaryIcon = false
+                }
+            }
+        }
+    }
+}
+
+struct TaskRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskRowView(task: .constant(Task(title: "My Task", category: .noCategory, isCompleted: false)))
+    }
+}
